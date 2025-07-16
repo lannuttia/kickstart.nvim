3cf84526cb25a85a963ab749fcf9fe5c91ae581e
Anthony T. Lannutti <alannutt@redhat.com>
2025-07-16 10:12:41 -0500

    add and utilize catppuccin-mocha colorscheme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,7 @@
 -- See the kickstart.nvim README for more information
 return {
   require 'custom.plugins.avante',
+  require 'custom.plugins.catppuccin',
   require 'custom.plugins.undotree',
   require 'custom.plugins.latex-live-preview',
 }
