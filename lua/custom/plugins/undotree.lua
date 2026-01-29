6520e23204b34980a2b39875558c463cb429010b
Anthony T. Lannutti <alannutt@redhat.com>
2026-01-29 10:04:50 -0600

    Update keybinding for undotree

--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -3,6 +3,6 @@ return {
   dependencies = 'nvim-lua/plenary.nvim',
   config = true,
   keys = { -- load the plugin only when using it's keybinding:
-    { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>" },
+    { '<leader>tu', "<cmd>lua require('undotree').toggle()<cr>", desc = '[T]oggle [U]ndotree' },
   },
 }
