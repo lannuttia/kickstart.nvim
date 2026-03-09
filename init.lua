78122df2e73751255d25296251e1a0c9dba9ed90
Anthony T. Lannutti <alannutt@redhat.com>
2026-03-09 14:38:22 -0500

    chore: add ts_ls

--- init.lua
+++ init.lua
@@ -615,6 +615,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
 
+        ts_ls = {},
         lemminx = {
           settings = {
             xml = {
